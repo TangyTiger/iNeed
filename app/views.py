5445edd2eb0e206a25821be09a80dd891ba142af
Sarthak Lodha <sarthaklodha15@gmail.com>
2020-08-18 12:34:51 -0700

    more stuff

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from app import app
+import random
 from flask import request, session, jsonify, render_template
 import logging
 
@@ -8,6 +9,8 @@ logger.setLevel(logging.ERROR)
 users = {
         'sarthaklodha15@gmail.com': '1234567890'
         }
+
+jobs = {}
 
 @app.route('/ineed', methods=['GET'])
 def ineed():
@@ -34,16 +37,38 @@ def create_account():
     if req['user'] in users:
         return jsonify({'response': 'This email is already in use.'})
     users[req['user']] = req['pass']
+    session['user'] = req['user']
     return jsonify({'response': 'Account Created!'})
 
 
 @app.route('/postjob', methods=['PUT'])
 def post_job():
+    req = request.json
+    pin = random.randint(10000, 99999)
+    while pin in jobs:
+        pin = random.randint(10000, 99999)
+    job = {
+        'title': req['title'],
+        'description': req['description'],
+        'tags': req['tags'],
+        'location': req['location'],
+        'name': req['employer'],
+        'pin': pin
+    }
+    jobs[str(pin)] = job
     return
 
 
 @app.route('/apply', methods=['PUT'])
 def apply():
+    req = request.json
+    application = {
+        'name': req['applicant'],
+        'bio': req['bio'],
+        'price': req['price'],
+        'email': req['email'],
+        'job': req['job']
+    }
     return
 
 
