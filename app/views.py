ec03f5af734f95ff0f835bf46e17a8f1db6b6f83
Sarthak Lodha <sarthaklodha15@gmail.com>
2020-08-23 15:10:09 -0700

    fixed homepage styles again

--- app/views.py
+++ app/views.py
@@ -6,9 +6,12 @@ import logging
 logger = logging.getLogger("werkzeug")
 logger.setLevel(logging.ERROR)
 
-userfile = 'users.json'
+users = {
+    'sarthaklodha15@gmail.com': '1234567890',
+    'aarush.uli@gmail.com': 'p00p'
+}
 
-jobfile = 'jobs.json'
+jobs = {}
 
 
 @app.route('/ineed', methods=['GET'])
@@ -21,16 +24,14 @@ def ineed():
 @app.route('/login', methods=['PUT'])
 def login():
     req = request.json
-    with open(userfile, 'r') as f:
-        users = json.load(f)
-        if req['email'] in users:
-            if req['pass'] == users[req['email']]:
-                session['email'] = req['email']
-                return jsonify({'response': 'you are logged in.'})
-            else:
-                return jsonify({'response': 'incorrect email or password'})
+    if req['email'] in users:
+        if req['pass'] == users[req['email']]:
+            session['email'] = req['email']
+            return jsonify({'response': 'you are logged in.'})
         else:
-            return jsonify({'response': 'this account does not exist'})
+            return jsonify({'response': 'incorrect email or password'})
+    else:
+        return jsonify({'response': 'this account does not exist'})
 
 
 @app.route('/get-create', methods=['GET'])
@@ -41,10 +42,9 @@ def create_account():
 @app.route('/createaccount', methods=['PUT'])
 def createaccount():
     req = request.json
-    with open(userfile, 'r+') as f:
-        users = json.load(f)
-        if req['email'] in users:
-            return 'This email is already in use'
+
+    if req['email'] in users:
+        return 'This email is already in use'
     users[req['email']] = req['pass']
     session['email'] = req['email']
     return 'Account Created!'
@@ -54,20 +54,16 @@ def createaccount():
 def post_job():
     req = request.json
     pin = random.randint(10000, 99999)
-    with open(jobfile, 'r+') as f:
-        jobs = json.load(f)
-        while str(pin) in jobs:
-            pin = random.randint(10000, 99999)
-        job = {
-            'title': req['title'],
-            'description': req['description'],
-            'county': req['county'],
-            'name': session['email'],
-            'pin': pin
-        }
-        jobs[str(pin)] = job
-        f.seek(0)
-        json.dump(jobs, f, indent=4)
+    while str(pin) in jobs:
+        pin = random.randint(10000, 99999)
+    job = {
+        'title': req['title'],
+        'description': req['description'],
+        'county': req['county'],
+        'name': session['email'],
+        'pin': pin
+    }
+    jobs[str(pin)] = job
     return "job created"
 
 
